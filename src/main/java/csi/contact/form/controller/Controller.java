a0142a71a83d958698c844b5b7f05c4ea4bbfb9c
Dmariano06 <djilanimariano@outlook.fr>
2023-12-10 00:38:04 +0100

    commit

---
+++ src/main/java/csi/contact/form/controller/Controller.java
@@ -0,0 +1,34 @@
+package csi.contact.form.controller;
+
+import csi.contact.form.entity.Entity;
+import csi.contact.form.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/contact")
+@CrossOrigin(origins = "*")
+public class Controller {
+    @Autowired
+    private Service service;
+
+    @PostMapping
+    public ResponseEntity<String> submitContactForm(@RequestBody Entity contact) {
+        // Ajoutez ici le code pour traiter les données du formulaire sans les stocker
+        System.out.println("Nom: " + contact.getName());
+
+        System.out.println("Email: " + contact.getEmail());
+        System.out.println("Message: " + contact.getMessage());
+
+        // Envoyer l'e-mail
+        String to = contact.getEmail(); // Remplacez par votre adresse e-mail
+        String subject = "Nouveau formulaire de contact";
+        String body = "Nom: " + contact.getName() + "\nEmail: " + contact.getEmail() + "\nMessage: " + contact.getMessage();
+
+        service.sendEmail(to, subject, body);
+
+        return ResponseEntity.ok("Formulaire soumis avec succès !");
+    }
+    }
